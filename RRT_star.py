ffe57c09153023e9d6dd8988d49b95518f0e6b25
ShinigamiRyuk47 <adityabharambe45@gmail.com>
2025-04-12 19:14:30 -0400

    udated RRT*

--- RRT_star.py
+++ RRT_star.py
@@ -7,15 +7,17 @@ from urdfpy import URDF
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 import os
+import math
 
-PI = np.pi
+PI = math.pi
 DOF = 6
 JOINT_LIMITS = []
 
 FIXED_URDF_PATH = os.path.expanduser("~/MAPF_CP_ws/src/ur10e_multi_arm_bringup/urdf/robot1.urdf")
 
 class Node:
-    def __init__(self, config, cost=0.0, parent=None):
+    def __init__(self, config, cost=0.0, parent=None, timestep=0):
+        self.timestep = timestep
         self.config = config
         self.cost = cost
         self.parent = parent
@@ -26,7 +28,7 @@ def config_distance(q1, q2):
 def interpolate_config(q1, q2, step_size):
     direction = q2 - q1
     length = np.linalg.norm(direction)
-    return q1 + (step_size / length) * direction if length > step_size else q2
+    return q1 + (step_size / length) * direction
 
 def backtrack_path(goal_node):
     path = []
@@ -47,28 +49,33 @@ def load_joint_limits_from_urdf():
         if joint.limit is not None and joint.joint_type == 'revolute':
             JOINT_LIMITS.append((joint.limit.lower, joint.limit.upper))
 
-def is_valid_configuration(config):
-    for i in range(DOF):
-        lower, upper = JOINT_LIMITS[i]
-        if config[i] < lower or config[i] > upper:
-            return False
+def IsValidArmConfiguration(config):
+    # Add here
     return True
+
+def build_constraint_table(constraints, agent, goal_q):
+    # Add here
+    pass
+
+def IsConstrained(curr_q, next_q, next_time, constraints):
+    # Add here
+    pass
 
 def extend_KDtree(tree, kdtree_data, q_rand, epsilon, step_size, radius):
     
     # Find the nearest neighbor to the random configuration
     kdtree = KDTree(kdtree_data)
     _, idx = kdtree.query(q_rand)
-    q_near = tree[idx]
+    nearest_node = tree[idx]
 
     # Extend the tree towards the random configuration
     q_new = []
-    q_prev = q_near.config.copy()
+    q_prev = nearest_node.config.copy()
     
     for step in range(step_size, epsilon + 1, step_size):
-        q_new = interpolate_config(q_near.config, q_rand, step)
+        q_new = interpolate_config(nearest_node.config, q_rand, step)
         
-        if not is_valid_configuration(q_new):
+        if not IsValidArmConfiguration(q_new):
             q_new = q_prev.copy()
             break
         else:
@@ -78,27 +85,27 @@ def extend_KDtree(tree, kdtree_data, q_rand, epsilon, step_size, radius):
             q_new = q_rand.copy()
             break
     
-    # Insert the new configuration into the tree
-    min_cost = q_near.cost + config_distance(q_near.config, q_new)
-    best_parent = q_near
+    # Finding the best parent for the new node
+    min_cost = nearest_node.cost + config_distance(nearest_node.config, q_new)
+    best_parent = nearest_node
     
     # Full KDTree-based radius search for rewiring
     indices = kdtree.query_ball_point(q_new, radius, p=2)
-    neighbors = [tree[i] for i in indices]
+    neighbor_nodes = [tree[i] for i in indices]
 
-    for node in neighbors:
+    for node in neighbor_nodes:
         cost = node.cost + config_distance(node.config, q_new)
         if cost < min_cost:
             best_parent = node
             min_cost = cost
 
     # Insert the new configuration into the tree
-    new_node = Node(q_new, cost=min_cost, parent=best_parent)
+    new_node = Node(q_new, cost=min_cost, parent=best_parent, timestep=best_parent.timestep + 1)
     tree.append(new_node)
-    kdtree_data.append(q_new.copy())
+    kdtree_data.append(new_node.config.copy())
 
-    # Rewire the neighbors
-    for node in neighbors:
+    # Update the cost of affected neighbor nodes
+    for node in neighbor_nodes:
         cost_through_new = new_node.cost + config_distance(new_node.config, node.config)
         if cost_through_new < node.cost:
             node.parent = new_node
@@ -106,7 +113,7 @@ def extend_KDtree(tree, kdtree_data, q_rand, epsilon, step_size, radius):
 
     return new_node, tree, kdtree_data
 
-def rrt_star_moveit(start_q, goal_q):
+def rrt_star_moveit(start_q, goal_q, agent, constraints):
     num_samples = 1000
     epsilon = 3
     step_size = 0.3
@@ -119,18 +126,28 @@ def rrt_star_moveit(start_q, goal_q):
     tree = [Node(start_q)]
     kdtree_data = [start_q.copy()]
     goal_node = None
+    
+    constraint_table = build_constraint_table(constraints, agent, goal_q)
 
     for _ in range(num_samples):
         q_rand = goal_q if random.random() < goal_bias else gen_rand_config()
+        
+        if not IsValidArmConfiguration(q_rand):
+            print("Invalid random configuration")
+            continue
+        
+        if IsConstrained(start_q, q_rand, 0, constraint_table):
+            print("Configuration is constrained")
+            continue
 
         newNode, tree, kdtree_data = extend_KDtree(tree, kdtree_data, q_rand, epsilon, step_size, radius)
 
-        if not is_valid_configuration(newNode):
+        if not IsValidArmConfiguration(newNode.config):
             print("Failed to extend KDTree")
             continue
 
         if config_distance(newNode.config, goal_q) < goal_threshold:
-            goal_node = Node(goal_q, parent=newNode, cost=newNode.cost + float(config_distance(newNode.config, goal_q)))
+            goal_node = Node(goal_q, parent=newNode, cost=newNode.cost + float(config_distance(newNode.config, goal_q)), timestep=newNode.timestep + 1)
             print("Found goal node")
             break
         
