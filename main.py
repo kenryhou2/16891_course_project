2d9e30e7e2c3b8b85547f7d570a0c8f2863b13a4
ShinigamiRyuk47 <adityabharambe45@gmail.com>
2025-04-20 22:45:43 -0400

    Comment out robot3 initialization and configuration in multiple UR5e scenario

--- main.py
+++ main.py
@@ -43,12 +43,13 @@ def run_multiple_ur5e_scenario():
 
         robot1 = UR5eRobot(position=[0, 0, 0], orientation=[0, 0, 0, 1])
         robot2 = UR5eRobot(position=[1, 0.75, 0], orientation=[0, 0, 1, 1])
-        robot3 = UR5eRobot(position=[1.1, -0.3, 0], orientation=[0, 0, 0.7071, 0.7071])
+        # robot3 = UR5eRobot(position=[1.1, -0.3, 0], orientation=[0, 0, 0.7071, 0.7071])
         # robot3 = UR5eRobot(position=[1.1, -0.3, 0], orientation=[0, 0, 0, 1])
+        
 
         robots.append(robot1)
         robots.append(robot2)
-        robots.append(robot3)
+        # robots.append(robot3)
 
     except Exception as e:
         logger.error(f"Failed to load UR5e robot: {str(e)}")
@@ -64,8 +65,8 @@ def run_multiple_ur5e_scenario():
     start_configs[robot2.robot_id] = [0, 0, 0, 0, 0, 0]
     goal_configs[robot2.robot_id] = [np.pi / 2, -np.pi / 3, np.pi / 6, -np.pi / 2, np.pi / 4, 0]
     
-    start_configs[robot3.robot_id] = [0, 0, 0, 0, 0, 0]
-    goal_configs[robot3.robot_id] = [np.pi / 4, -np.pi / 6, np.pi / 2, -np.pi / 2, np.pi / 4, 0]
+    # start_configs[robot3.robot_id] = [0, 0, 0, 0, 0, 0]
+    # goal_configs[robot3.robot_id] = [np.pi / 4, -np.pi / 6, np.pi / 2, -np.pi / 2, np.pi / 4, 0]
 
     planners = {}
     for robot in robots:
